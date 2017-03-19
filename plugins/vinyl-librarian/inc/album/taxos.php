0cdb449488fe6cfe646981808d7be4c384f15572
Bronson Quick <bronson@sennza.com.au>
2017-03-19 12:08:59 +1100

    Add our taxonomies

--- plugins/vinyl-librarian/inc/album/taxos.php
+++ plugins/vinyl-librarian/inc/album/taxos.php
@@ -2,9 +2,15 @@
 namespace Vinyl_Librarian\Album\Taxos;
 
 function bootstrap() {
-	add_action( 'plugins_loaded', 'register_taxos' );
+	add_action( 'init', __NAMESPACE__ . '\\register_taxos' );
 }
 
 function register_taxos() {
+	register_extended_taxonomy(
+		'genre',
+		'album',
+		[ 'meta_box' => 'radio' ]
+	);
 
+	register_extended_taxonomy( 'style', 'album' );
 }
