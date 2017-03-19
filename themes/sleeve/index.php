b1c2eff0cbc69b2da0f8dcd46dde0d83e96591be
Bronson Quick <bronson@sennza.com.au>
2017-03-20 10:39:27 +1100

    Start our Vinyl theme

---
+++ themes/sleeve/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Vinyl Librarian</title>
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
+</head>
+<body>
+	<select class="js-data-example-ajax">
+		<option value="3620194" selected="selected">select2/select2</option>
+	</select>
+
+</body>
+</html>
