f84c26d12c436760a8645d375b8947d44fda2ef7
Bronson Quick <bronson@sennza.com.au>
2017-03-19 18:14:08 +1100

    Add meta data to REST endpoints

--- plugins/vinyl-librarian/inc/album/custom-meta-boxes.php
+++ plugins/vinyl-librarian/inc/album/custom-meta-boxes.php
@@ -3,6 +3,7 @@ namespace Vinyl_Librarian\Album\Custom_Meta_Boxes;
 
 function bootstrap() {
 	add_filter( 'cmb_meta_boxes', __NAMESPACE__ . '\\album_cmbs' );
+	add_action( 'rest_api_init',  __NAMESPACE__ . '\\album_rest_data' );
 }
 
 function album_cmbs( $meta_boxes ) {
@@ -30,4 +31,31 @@ function album_cmbs( $meta_boxes ) {
 	);
 
 	return $meta_boxes;
+}
+
+function album_rest_data() {
+	register_rest_field( 'album',
+		'year',
+		array(
+			'get_callback'    => __NAMESPACE__ . '\\get_year',
+			'update_callback' => null,
+			'schema'          => null,
+		)
+	);
+	register_rest_field( 'album',
+		'label',
+		array(
+			'get_callback'    => __NAMESPACE__ . '\\get_label',
+			'update_callback' => null,
+			'schema'          => null,
+		)
+	);
+}
+
+function get_year( $post, $field_name, $request ) {
+	return get_post_meta( $post['id'], 'year', true );
+}
+
+function get_label( $post, $field_name, $request ) {
+	return get_post_meta( $post['id'], 'label', true );
 }
